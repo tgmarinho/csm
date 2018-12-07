30d01b146d16046ade14a7080f1f24ce4a1fc885
Thiago Marinho <tgmarinho@gmail.com>
2018-12-07 20:35:17 -0300

    Updates

---
+++ precache-manifest.cf2d587e4213e0c4f8b461fdaedb54c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9493b6514e2ff2dc3ffa",
+    "url": "/static/js/main.9493b651.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "0b47ca38df1ccf03210e61ee1c1c0140",
+    "url": "/index.html"
+  }
+];
